1a42d8be3d68e0a4e0fc8552c29e92df11d122da
Poli404 <ra134539@uem.br>
2025-10-29 10:36:56 -0300

    parser funciona

--- parser.py
+++ parser.py
@@ -2,15 +2,15 @@
 import sys
 import ply.yacc as yacc
 from lexer import tokens, make_lexer
-from ast_calculadin import *
 
 precedence = (
-    ("nonassoc", "IF", "THEN", "ELSE"),
+    ("nonassoc", "IFS"),
+    ("nonassoc", "ELSE"),
     ("right", "NOT"),
     ("nonassoc", "=", "DIF", "<", "LT", ">", "GT"),
     ("left", '+', '-'),
 	("left", '*', 'DIV'),
-    ("right", 'UMENOS')
+    ("right", 'NEG')
 )
 
 # Regras gramaticais
@@ -19,20 +19,20 @@ def p_programa(p):
     pass
 
 def p_bloco(p):
-    """bloco: sec_declarar_vars sec_declarar_subrots cmd_composto"""
+    """bloco : sec_declarar_vars sec_declarar_subrots cmd_composto"""
     pass
 
 def p_sec_declarar_vars(p):
     """sec_declarar_vars : VAR declarar_vars ';' opcional_dec_vars"""
     pass
-def p_sec_declarar_vars_vazia(p):
+def p_sec_declarar_vars_vazio(p):
     """sec_declarar_vars : """
     pass
 
 def p_opcional_dec_vars(p):
-    """opcional_dec_vars : declarar_vars ';'"""
+    """opcional_dec_vars : declarar_vars ';' opcional_dec_vars"""
     pass
-def p_opcional_dec_vars_vazia(p):
+def p_opcional_dec_vars_vazio(p):
     """opcional_dec_vars : """
     pass
 
@@ -44,21 +44,22 @@ def p_lista_ids(p):
     pass
 
 def p_opcional_ids(p):
-    """opcional_ids : ',' ID"""
+    """opcional_ids : ',' ID opcional_ids"""
     pass
-def p_opcional_ids_vazia(p):
+def p_opcional_ids_vazio(p):
     """opcional_ids : """
     pass
 
 def p_tipo(p):
-    """tipo : BOOL | INT"""
+    """tipo : BOOL
+            | INT"""
     pass
 
 def p_sec_declarar_subrots(p):
     """sec_declarar_subrots : declarar_proc ';'
                             | declarar_func ';'"""
     pass
-def p_sec_declarar_subrots_vazia(p):
+def p_sec_declarar_subrots_vazio(p):
     """sec_declarar_subrots : """
     pass
 
@@ -77,14 +78,14 @@ def p_bloco_subrot(p):
 def p_param_formais(p):
     """param_formais : '(' dec_params opcional_dec_params ')'"""
     pass
-def p_param_formais_vazia(p):
+def p_param_formais_vazio(p):
     """param_formais : """
     pass
 
 def p_opcional_dec_params(p):
-    """opcional_dec_params : ';' dec_params"""
+    """opcional_dec_params : ';' dec_params opcional_dec_params"""
     pass
-def p_opcional_dec_params_vazia(p):
+def p_opcional_dec_params_vazio(p):
     """opcional_dec_params : """
     pass
 
@@ -97,14 +98,20 @@ def p_cmd_composto(p):
     pass
 
 def p_opcional_cmd(p):
-    """opcional_cmd : ';' cmd"""
+    """opcional_cmd : ';' cmd opcional_cmd"""
     pass
-def p_opcional_cmd_vazia(p):
+def p_opcional_cmd_vazio(p):
     """opcional_cmd : """
     pass
 
 def p_cmd(p):
-    """cmd : atribuicao | chamada_proc | condicional | repeticao | leitura | escrita | cmd_composto"""
+    """cmd : atribuicao
+            | chamada_proc
+            | condicional
+            | repeticao
+            | leitura
+            | escrita
+            | cmd_composto"""
     pass
 
 def p_atribuicao(p):
@@ -115,15 +122,12 @@ def p_chamada_proc(p):
     """chamada_proc : ID '(' lista_expr ')'"""
     pass
 
-def p_condicional(p):
-    """condicional : IF expr THEN cmd opcional_else"""
+def p_condicional_simples(p):
+    """condicional : IF expr THEN cmd %prec IFS"""
     pass
 
-def p_opcional_else(p):
-    """opcional_else : ELSE cmd"""
-    pass
-def p_opcional_else_vazia(p):
-    """opcional_else : """
+def p_condicional_else(p):
+    """condicional : IF expr THEN cmd ELSE cmd"""
     pass
 
 def p_repeticao(p):
@@ -141,14 +145,14 @@ def p_escrita(p):
 def p_lista_expr(p):
     """lista_expr : expr opcional_expr"""
     pass
-def p_lista_expr_vazia(p):
-    """list_expr : """
+def p_lista_expr_vazio(p):
+    """lista_expr : """
     pass
 
 def p_opcional_expr(p):
-    """opcional_expr : ',' expr"""
+    """opcional_expr : ',' expr opcional_expr"""
     pass
-def p_opcional_expr_vazia(p):
+def p_opcional_expr_vazio(p):
     """opcional_expr : """
     pass
 
@@ -159,12 +163,17 @@ def p_expr(p):
 def p_opcional_relacao(p):
     """opcional_relacao : relacao expr_simples"""
     pass
-def p_opcional_relacao_vazia(p):
+def p_opcional_relacao_vazio(p):
     """opcional_relacao : """
     pass
 
 def p_relacao(p):
-    """relacao : '=' | '<>' | '<' | '<=' | '>' | '>='"""
+    """relacao : '='
+                | DIF
+                | '<'
+                | LT
+                | '>'
+                | GT"""
     pass
 
 def p_expr_simples(p):
@@ -172,9 +181,11 @@ def p_expr_simples(p):
     pass
 
 def p_opcional_termo(p):
-    """opcional_termo : '+' termo | '-' termo | OR termo"""
+    """opcional_termo : '+' termo opcional_termo
+                        | '-' termo opcional_termo
+                        | OR termo opcional_termo"""
     pass
-def p_opcional_termo_vazia(p):
+def p_opcional_termo_vazio(p):
     """opcional_termo : """
     pass
 
@@ -182,12 +193,36 @@ def p_termo(p):
     """termo : fator opcional_fator"""
     pass
 
-def opcional_termo(p):
-    """opcional_termo : '*' fator | DIV fator | AND fator"""
+def p_opcional_fator(p):
+    """opcional_fator : '*' fator opcional_fator
+                        | DIV fator opcional_fator
+                        | AND fator opcional_fator"""
     pass
-def opcional_termo_vazia(p):
-    """opcional_termo : """
+def p_opcional_fator_vazio(p):
+    """opcional_fator : """
     pass
+
+def p_fator(p):
+    """fator : variavel
+            | NUM
+            | logico
+            | chamada_funcao
+            | '(' expr ')'
+            | NOT fator
+            | NEG fator"""
+    pass
+
+def p_variavel(p):
+    """variavel : ID"""
+    pass
+
+def p_logico(p):
+    """logico : TRUE
+              | FALSE"""
+    pass
+
+def p_chamada_fucao(p):
+    """chamada_funcao : ID '(' lista_expr ')'"""
 
 # Erro sintático
 def p_error(tok):
@@ -200,9 +235,8 @@ def p_error(tok):
 def make_parser():
     return yacc.yacc(start="programa")
 
-# Para testar o parser: python3 parser.py <exemplo.calc
+# Para testar o parser: python3 parser.py < exemplo.ras
 if __name__ == "__main__":
     data = sys.stdin.read()
     parser = make_parser()
     parser.parse(data, lexer=make_lexer())
-    print()
