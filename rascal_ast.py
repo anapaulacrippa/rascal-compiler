100e0c3af35cd2f7b854ef56bb9de261b9aa4879
anapaulacrippa <aplcrippa@gmail.com>
2025-11-26 01:38:42 -0300

    completa construcao de nós da ast

--- rascal_ast.py
+++ rascal_ast.py
@@ -1,17 +1,65 @@
-from dataclasses import dataclass
-from typing import Optional
+from __future__ import annotations
+from dataclasses import dataclass, field
+from typing import List, Optional
+from rascal_defs import Simbolo, Tipo
 
 class No:...
 
 class Comando(No):...
 
 class Expr(No):...
+    # tipo: Tipo | None = None
 
-# --- Expressão ---
+# --- Estrutura geral do programa ---
+
+@dataclass
+class Programa(No):
+    nome: Id
+    bloco: Bloco
+    total_vars: int = 0
+    
+@dataclass
+class Bloco(No):
+    """
+    Pode conter declarações ou comandos.
+    """
+    secao_variaveis: List[DeclaracaoVar] = field(default_factory=list)
+    secao_subrotinas: List[DeclaracaoProc | DeclaracaoFunc] = field(default_factory=list)
+    cmd_composto: CmdComposto | None = None
+
+# --- Declarações ---
+
+@dataclass
+class DeclaracaoVar(No):
+    lista_ids: List[Id]
+    tipo: Tipo # ou tipo: str?
+
+@dataclass
+class DeclaracaoProc(No):
+    id: Id
+    params: List[ParamsFormais]
+    bloco: Bloco
+
+@dataclass
+class DeclaracaoFunc(No):
+    id: Id
+    params: List[ParamsFormais]
+    tipo_retorno: Tipo
+    bloco: Bloco
+
+@dataclass
+class ParamsFormais(No):
+    lista_ids: List[Id]
+    tipo: Tipo
+    # passagem por valor ou por referencia
+    por_referencia: bool = False
+
+# --- Expressões ---
 
 @dataclass
 class Id(Expr):
-    identificador: str
+    nome: str
+    simbolo: Simbolo | None = None
 
 @dataclass
 class Num(Expr):
@@ -22,43 +70,37 @@ class Bool(Expr):
 
 @dataclass
 class ExprBin(Expr):
+    """
+    +, -, *, div, and, or, =, <>, <, <=, >, >=
+    """
     op: str
     esq: Expr
     dir: Expr
 
 @dataclass
 class ExprUn(Expr):
+    """
+    -, not
+    """
     op: str
     expr: Expr
 
 @dataclass
-class ChamadaFuncao(Expr):
+class ChamadaFunc(Expr):
     nome: Id
-    argumentos: list[Expr]
+    argumentos: List[Expr]
 
-# --- Declaração ---
-
-@dataclass
-class Tipo(No):
-    """
-    Tipos possíveis: *integer* ou *boolean*.
-    """
-    nome: str
-
-@dataclass
-class Declaracao(No):
-    """
-    Declaração de variáveis ou parâmetros.
-    """
-    lista_ids: list[Id]
-    tipo: Tipo
-
-# --- Comando ---
+# --- Comandos ---
 
 @dataclass
 class Atribuicao(Comando):
     id: Id
     expr: Expr
+
+@dataclass
+class ChamadaProc(Comando):
+    nome: Id
+    argumentos: List[Expr]
 
 @dataclass
 class Condicional(Comando):
@@ -73,19 +115,12 @@ class Repeticao(Comando):
 
 @dataclass
 class Leitura(Comando):
-    variaveis: list[Id]
+    variaveis: List[Id]
 
 @dataclass
 class Escrita(Comando):
-    expressoes: list[Expr]
-
-# --- Estrutura ---
+    expressoes: List[Expr]
 
 @dataclass
-class Bloco(No):
-    pass
-
-@dataclass
-class Programa(No):
-    nome: Id
-    bloco: Bloco
+class CmdComposto(Comando):
+    lista_cmds: List[Comando]
